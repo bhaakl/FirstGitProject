c92d70db278cd6f9d6b1df5927cf54f69b6f56bf
Baiysh Begaliev <khatakii@ya.ru>
2022-07-10 18:50:22 +0600

    Create UseCases and Repository class

--- app/src/main/java/com/example/firstgitproject/presentation/MainActivity.kt
+++ app/src/main/java/com/example/firstgitproject/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.firstgitproject
+package com.example.firstgitproject.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firstgitproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
