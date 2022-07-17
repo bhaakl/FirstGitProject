f8d00087eeb030db04a626de23068d7236fb0bb2
Baiysh Begaliev <khatakii@ya.ru>
2022-07-17 19:00:46 +0600

    Create UseCases and other classes for the app to work. As well as create basic GUI app.

---
+++ app/src/main/java/com/example/firstgitproject/domain/EditShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.firstgitproject.domain
+
+class EditShopItemUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun editShopItem(shopItem: ShopItem) {
+        shopListRepository.editShopItem(shopItem)
+    }
+}
---
+++ app/src/main/java/com/example/firstgitproject/presentation/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.firstgitproject.presentation
+
+import androidx.lifecycle.ViewModel
+import com.example.firstgitproject.data.ShopListRepositoryImpl
+import com.example.firstgitproject.domain.DeleteShopItemUseCase
+import com.example.firstgitproject.domain.EditShopItemUseCase
+import com.example.firstgitproject.domain.GetShopListUseCase
+import com.example.firstgitproject.domain.ShopItem
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = getShopListUseCase.getShopList()
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+    }
+}
