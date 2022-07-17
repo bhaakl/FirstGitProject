1eda2da9d0a9b181a7c9838ba1814c53682429b0
Baiysh Begaliev <khatakii@ya.ru>
2022-07-17 18:55:58 +0600

    Create UseCases and other classes for the app to work. As well as create basic GUI app.

--- app/src/main/java/com/example/firstgitproject/presentation/MainActivity.kt
+++ app/src/main/java/com/example/firstgitproject/presentation/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.firstgitproject.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.firstgitproject.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: ShopListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecyclerView()
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.shopList.observe(this) {
+            adapter.shopList = it
+        }
+    }
+
+    private fun setupRecyclerView() {
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        adapter = ShopListAdapter()
+        rvShopList.adapter = adapter
     }
 }
